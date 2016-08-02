08ceb83ba0ba5349e82a8af79ab617ec48c49fbe
isierra <isierra@jgingenieros.es>
2016-08-02 10:22:28 +0200

    Added Roboto
    Added Logo to app-shell

--- src/shell/shell.redux.ts
+++ src/shell/shell.redux.ts
@@ -46,45 +46,55 @@ const defaultAppShellState: AppShell = {
     windowSize: { width: 1024, height: 768 },
 };
 
+function setNavigation(state: AppShell, isOpen: boolean) {
+    if (state.isNavigationOpen !== isOpen) {
+        return Object.assign({}, state, {
+            isNavigationOpen: isOpen,
+            isControlNavigationOpen: state.isControlNavigationOpen && !isOpen
+        });
+    }
+    return state;
+}
+
+function setControlNavigation(state: AppShell, isOpen: boolean) {
+    if (state.isControlNavigationOpen !== isOpen) {
+        return Object.assign({}, state, {
+            isControlNavigationOpen: isOpen,
+            isNavigationOpen: state.isNavigationOpen && !isOpen
+        });
+    }
+    return state;
+}
+
+function setDevTools(state: AppShell, isOpen: boolean) {
+    isOpen = isOpen && state.isDevelopment && !state.isProduction;
+    if (state.isDevToolsOpen !== isOpen) {
+        return Object.assign({}, state, {
+            isDevToolsOpen: isOpen
+        });
+    }
+    return state;
+}
+
+function setWindowSize(state: AppShell, size: { width: number, height: number }) {
+    return Object.assign({}, state, { windowSize: size });
+}
+
 export const appShellReducer: ActionReducer<AppShell> = (state: AppShell, action: Action) => {
     if (!state) { state = defaultAppShellState; }
 
     switch (action.type) {
-        case AppShellActionTypes.TOGGLE_DEVTOOLS:
-            return Object.assign({}, state, { isDevToolsOpen: !state.isDevToolsOpen });
+        case AppShellActionTypes.TOGGLE_DEVTOOLS: return setDevTools(state, !state.isDevToolsOpen);
 
-        case AppShellActionTypes.TOGGLE_NAVIGATION:
-            return Object.assign({}, state, { isNavigationOpen: !state.isNavigationOpen });
+        case AppShellActionTypes.TOGGLE_NAVIGATION: return setNavigation(state, !state.isNavigationOpen);
+        case AppShellActionTypes.OPEN_NAVIGATION: return setNavigation(state, true);
+        case AppShellActionTypes.CLOSE_NAVIGATION: return setNavigation(state, false);
 
-        case AppShellActionTypes.OPEN_NAVIGATION:
-            if (!state.isNavigationOpen) {
-                return Object.assign({}, state, { isNavigationOpen: true });
-            }
-            break;
+        case AppShellActionTypes.TOGGLE_CONTROL_NAVIGATION: return setControlNavigation(state, !state.isControlNavigationOpen);
+        case AppShellActionTypes.OPEN_CONTROL_NAVIGATION: return setControlNavigation(state, true);
+        case AppShellActionTypes.CLOSE_CONTROL_NAVIGATION: return setControlNavigation(state, false);
 
-        case AppShellActionTypes.CLOSE_NAVIGATION:
-            if (state.isNavigationOpen) {
-                return Object.assign({}, state, { isNavigationOpen: false });
-            }
-            break;
-
-        case AppShellActionTypes.TOGGLE_CONTROL_NAVIGATION:
-            return Object.assign({}, state, { isControlNavigationOpen: !state.isControlNavigationOpen });
-
-        case AppShellActionTypes.OPEN_CONTROL_NAVIGATION:
-            if (!state.isControlNavigationOpen) {
-                return Object.assign({}, state, { isControlNavigationOpen: true });
-            }
-            break;
-
-        case AppShellActionTypes.CLOSE_CONTROL_NAVIGATION:
-            if (state.isControlNavigationOpen) {
-                return Object.assign({}, state, { isControlNavigationOpen: false });
-            }
-            break;
-
-        case AppShellActionTypes.SET_WINDOW_SIZE:
-            return Object.assign({}, state, { windowSize: action.payload });
+        case AppShellActionTypes.SET_WINDOW_SIZE: return setWindowSize(state, action.payload);
     }
     return state;
 };
